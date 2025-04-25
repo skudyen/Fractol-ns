672e887136c92ff532253b0342673e3c058a7a4d
Natthasit Suwaphrom <nsuwaphr@z2t11c2.42bangkok.com>
2025-04-25 20:26:04 +0700

    frist commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+NAME    = fractol
+
+SOURCES = src/color.c src/events.c src/fractol.c src/
+
+CC      = gcc
+CFLAGS  = -Wall -Wextra -Werror
+
+LIB_PATH
+LIB_NA
---
+++ includes/fractol.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsuwaphr <nsuwaphr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 16:17:57 by nsuwaphr          #+#    #+#             */
+/*   Updated: 2025/04/25 16:53:15 by nsuwaphr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../MLX42/include/MLX42/MLX42_Int.h"
+# include <math.h>
+
+# define WIDTH 800
+# define HEIGHT 800
+
+# define MANDELBROT 1
+# define JULIA 2
+# define TRICORN 3
+
+#endif
